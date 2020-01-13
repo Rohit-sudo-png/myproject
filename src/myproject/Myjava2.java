8012c77b8b7818654eefc0a39a8492fbb1cc9d8f
rohit <rohit@Rohit>
2020-01-14 01:29:03 +0800

    added additional chk

--- src/myproject/Myjava2.java
+++ src/myproject/Myjava2.java
@@ -5,6 +5,7 @@ public class Myjava2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World   i am either developer or tester");
+		System.out.println("added one more  i am either developer or tester");
 	}
 
 }
